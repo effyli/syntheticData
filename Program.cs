5b4797d21ba92ace8a1c27afb6e89538bc5696ea
Effy Li <t-lixue@microsoft.com>
2019-03-13 14:09:16 +0000

    Change the version of json

--- Program.cs
+++ Program.cs
@@ -13,22 +13,27 @@ namespace Synthetic
         static void Main()
         {
             string json = @"{
-               'CPU': 'Intel',
-               'PSU': '500W',
-               'Drives': [
-                 'DVD read/writer'
-                 /*(broken)*/,
-                 '500 gigabyte hard drive',
-                 '200 gigabyte hard drive'
-               ]
-            }";
+            'CPU': 'Intel',
+            'PSU': '500W',
+            'Drives': [
+                'DVD read/writer'
+                /*(broken)*/,
+                '500 gigabyte hard drive',
+                '200 gigabyte hard drive'
+            ]
+        }";
+
             JsonTextReader reader = new JsonTextReader(new StringReader(json));
-            if (reader.HasLineInfo())
+            while (reader.Read())
             {
-                reader.Read();
-            }
-            else {
-                reader.Close();
+                if (reader.Value != null)
+                {
+                    Console.WriteLine("Token: {0}, Value: {1}", reader.TokenType, reader.Value);
+                }
+                else
+                {
+                    Console.WriteLine("Token: {0}", reader.TokenType);
+                }
             }
 
         }
